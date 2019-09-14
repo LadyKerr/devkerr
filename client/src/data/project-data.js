ce30cf1b427789f8bf1a2cf53c65477d342f61ee
Kedasha <kadiekerr@gmail.com>
2019-09-14 23:41:43 +0000

    project data

---
+++ client/src/data/project-data.js
@@ -0,0 +1,46 @@
+const projects = [
+  {
+    id: 1,
+    title: "Future Hope - School in the Sky",
+    role: "Full-Stack Developer",
+    technologies: "React, React Hooks, Redux, Firebase, Full Calendar",
+    description: "Future Hope School connects teachers in Ghana to retired teachers in North America. Our mission is to increase school retention rates in Ghana",
+    projectImage: "https://source.unsplash.com/random",
+    projectURL: "",
+    githubRepo: ""
+  },
+  {
+    id: 2,
+    title: "Track Your Coin",
+    role: "Full-Stack Developer",
+    technologies: "NodeJs, ExpressJs, Knex, Jest, BcryptJS, SQLite3, Postgres, Rect,React Hooks, Redux",
+    description: "Track Your Coin allows you to manage your expenses and gain control of your finances",
+    projectImage: "https://source.unsplash.com/random",
+    projectURL: "",
+    githubRepo: ""
+  },
+  {
+    id: 3,
+    title: "HairCare",
+    role: "Front-End Architect",
+    technologies: "React, React-Router, Redux, Axios, Material-UI",
+    description:
+      "HairCare is a web application that connects hairstylists with potential clients.",
+    projectImage: "https://source.unsplash.com/random",
+    projectURL: "",
+    githubRepo: "https://github.com/haircare-bw/Front-End-Kerr"
+  },
+  {
+    id: 4,
+    title: "Luncher",
+    role: "UI Developer",
+    technologies: "CSS, LESS, Semantic HTML, Javascript",
+    description:
+      "Luncher is a web application that allows schools to request funds from donors for their school lunch programs",
+    projectImage: "https://source.unsplash.com/random",
+    projectURL: "https://ladykerr.github.io/Luncher-App/index.html",
+    githubRepo: "https://github.com/LadyKerr/Luncher-App"
+  }
+];
+
+export default projects;
