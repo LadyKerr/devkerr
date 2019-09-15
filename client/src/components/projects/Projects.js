70106cdcd354e0620d571b41664431f7c0206cd4
Kedasha <kadiekerr@gmail.com>
2019-09-15 19:49:10 +0000

    made font bolder

--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -14,6 +14,7 @@ import Collapse from '@material-ui/core/Collapse';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import projectData from "../../data/project-data";
+import "./projects.css";
 
 const useStyles = makeStyles(theme => ({
   cardGrid: {
