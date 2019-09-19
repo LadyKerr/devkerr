4421c5b71b48ff6bef24d077c2d945d478f9e040
LadyKerr <kadiekerr@gmail.com>
2019-09-19 11:38:25 -0400

    added projects photos

--- client/src/data/project-data.js
+++ client/src/data/project-data.js
@@ -1,21 +1,29 @@
+import futurehope from "../../public/img/futurehope.jpg";
+import luncher from "../../public/img/luncher.jpg";
+import trackyourcoin from "../../public/img/trackyourcoin.jpg";
+
 const projects = [
   {
     id: 1,
     title: "Future Hope School",
     role: "Full-Stack Developer",
     technologies: "React, React Hooks, Redux, Firebase, Full Calendar",
-    description: "Future Hope School connects teachers in Ghana to retired teachers in North America. Our mission is to increase school retention rates in Ghana",
-    projectImage: "https://source.unsplash.com/random",
+    description:
+      "Future Hope School connects teachers in Ghana to retired teachers in North America. Our mission is to increase school retention rates in Ghana",
+    projectImage: futurehope,
     projectURL: "https://staging.futurehopeschool.com/",
-    githubRepo: "https://github.com/labs-future-hope/Future-Hope-Client/tree/Staging"
+    githubRepo:
+      "https://github.com/labs-future-hope/Future-Hope-Client/tree/Staging"
   },
   {
     id: 2,
     title: "Track Your Coin",
     role: "Full-Stack Developer",
-    technologies: "NodeJs, ExpressJs, Knex, Jest, BcryptJS, SQLite3, Postgres, Rect,React Hooks, Redux",
-    description: "Track Your Coin allows you to manage your expenses and gain control of your finances.",
-    projectImage: "https://source.unsplash.com/random",
+    technologies:
+      "NodeJs, ExpressJs, Knex, Jest, BcryptJS, SQLite3, Postgres, Rect,React Hooks, Redux",
+    description:
+      "Track Your Coin allows you to manage your expenses and gain control of your finances.",
+    projectImage: trackyourcoin,
     projectURL: "",
     githubRepo: "https://github.com/Track-Your-Coins"
   },
@@ -26,7 +34,7 @@ const projects = [
     technologies: "CSS, LESS, Semantic HTML, Javascript",
     description:
       "Luncher is a web application that allows schools to request funds from donors for their school lunch programs. A simple landing page was built.",
-    projectImage: "https://source.unsplash.com/random",
+    projectImage: luncher,
     projectURL: "https://ladykerr.github.io/Luncher-App/index.html",
     githubRepo: "https://github.com/LadyKerr/Luncher-App"
   }
